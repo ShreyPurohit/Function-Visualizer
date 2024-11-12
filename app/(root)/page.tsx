d7dd6b7eb19ce51dedc11788449d7ec2e68a8d2b
Shrey Purohit <purohitshrey9@gmail.com>
2024-11-12 19:12:54 +0530

    Update: Tailwind Styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 export default function Home() {
   return (
-    <section className="blue_container !min-h-[230px]">
-      <h1 className="heading rounded-md">
-        Visualize Your Functions, <br />
-        Understand Your Code
-      </h1>
-      <p className="sub-heading !max-w-3xl">Simplify Anonymous Functions By Visualizing</p>
-    </section>
+    <>
+      <section className="blue_container !min-h-[230px]">
+        <h1 className="heading rounded-md">
+          Visualize Your Functions, <br />
+          Understand Your Code
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Simplify Anonymous Functions By Visualizing
+        </p>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">Paste Your Function Here:</p>
+      </section>
+    </>
   );
 }
